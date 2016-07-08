bf0135065628527e8d1836e29f2d7764cf58e37b
SuganyaVS <sugavaithee@gmail.com>
2016-07-08 15:58:37 +0530

    Update and rename README.md to poss.c

---
+++ poss.c
@@ -0,0 +1,19 @@
+# POS
+#include<stdio.h>
+main()
+{
+int a;
+printf("\n enter a number");
+scanf("%d",&a);
+if(a>0&&a<0)
+{
+if(a>0)
+printf("\n given number is possitive");
+else
+printf("\n given number is negative");
+}
+else 
+{
+printf("given number is zero");
+}
+}
