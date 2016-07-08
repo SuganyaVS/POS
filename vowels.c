6ba16fec1ec244788b55d4ee0120ee3b0273c748
SuganyaVS <sugavaithee@gmail.com>
2016-07-08 16:26:36 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+char c;
+printf("\n enter a char");
+scanf("%c",&c);
+if(c==a&&c==e&&c==i&&c==o&&c==u)
+printf("given char is vowels");
+else
+printf("not a vowel");
+}
